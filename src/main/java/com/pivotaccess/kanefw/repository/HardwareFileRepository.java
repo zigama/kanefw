e70a348f7c2af31526f82fec7281e31b15075b5a
Aidspan <zigdidier@gmail.com>
2019-03-29 12:49:35 +0200

    HF repo

--- src/main/java/com/pivotaccess/kanefw/repository/HardwareFileRepository.java
+++ src/main/java/com/pivotaccess/kanefw/repository/HardwareFileRepository.java
@@ -1,7 +1,12 @@
 package com.pivotaccess.kanefw.repository;
 
+import com.pivotaccess.kanefw.domain.Hardware;
 import com.pivotaccess.kanefw.domain.HardwareFile;
+import com.pivotaccess.kanefw.domain.enumeration.FileCategory;
+import com.pivotaccess.kanefw.domain.enumeration.FileStatus;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,5 +16,10 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface HardwareFileRepository extends JpaRepository<HardwareFile, Long> {
+	
+	@Query("SELECT hardwareFile FROM HardwareFile hardwareFile"
+			+ " WHERE hardwareFile.hardware =:hardware AND hardwareFile.category =:category"
+			+ " ORDER BY hardwareFile.version, hardwareFile.dateUploaded DESC")
+	HardwareFile findOneByHardwareAndCategoryOrderByVersionDateUploadedDesc( Hardware hardware, FileCategory category);
 
 }
